796ec9e7a8cb9e6152a3481ff971921bf6b5747a
Daniel Roa <droa@apgslt.com>
2023-09-09 11:16:23 -0500

    Avance Querys - DR

--- src/main/java/GrupoF/Proyecto3/Repositorios/UsuarioRepositorio.java
+++ src/main/java/GrupoF/Proyecto3/Repositorios/UsuarioRepositorio.java
@@ -1,10 +1,13 @@
-package GrupoF.Proyecto3.repositorios;
+package GrupoF.Proyecto3.Repositorios;
 
 import GrupoF.Proyecto3.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, String>  {
-    
+    @Query("SELECT u FROM Usuario u WHERE u.correo = :correo")
+    public Usuario buscarPorCorreo(@Param("correo") String correo);    
 }
