ce895d57b666688f21c588de6a290f2ca5e1d208
Dai <118862708+d4ivc@users.noreply.github.com>
2023-09-09 23:55:52 -0300

    agregado al servicio cliente: listar, actualizar y baja. -Dai.

--- src/main/java/GrupoF/Proyecto3/Servicios/ProveedorServicio.java
+++ src/main/java/GrupoF/Proyecto3/Servicios/ProveedorServicio.java
@@ -16,7 +16,7 @@ public class ProveedorServicio {
     @Transactional
     public void registrarProveedor (String nombreApellido, String contrasenia, Integer dni, String correo, Integer telefono, Integer numeroMatricula, String categoriaServicio, Double costoHora){
         
-        validarP(nombreApellido, contrasenia, correo, telefono, numeroMatricula, categoriaServicio, costoHora);
+        validarP(nombreApellido, contrasenia, dni, correo, telefono, numeroMatricula, categoriaServicio, costoHora);
              
         if (pr.findByCorreo(correo) != null){
             throw new Exception("Ya existe un usuario registrado con este correo electrónico.");
@@ -30,7 +30,8 @@ public class ProveedorServicio {
         proveedor.setCorreo(correo);
         proveedor.setTelefono(telefono);
         proveedor.setnumeroMatricula(numeroMatricula);
-        
+        proveedor.setCategoriaMatricula(categoriaMatricula);
+        proveedor.setCostoHora(costaHora);
         proveedor.serAlta(true);
         proveedor.setRol(Rol.USUARIO);
         
@@ -38,13 +39,16 @@ public class ProveedorServicio {
               
     }
     
-    private void validarP(String nombreApellido, String contrasenia, String correo, Integer telefono, Integer numeroMatricula, String categoriaServicio, Double costoHora) throws Exception{
+    private void validarP(String nombreApellido, String contrasenia, Integer dni, String correo, Integer telefono, Integer numeroMatricula, String categoriaServicio, Double costoHora) throws Exception{
 
         if (nombreApellido.isEmpty() || nombreApellido == null) {
             throw new Exception("El nombre y apellido no pueden ser nulos o estar vacíos");
         }
         if (contrasenia.isEmpty() || contrasenia == null || contrasenia.length() <= 8) {
             throw new Exception("La contraseña no puede estar vacía, y tener más de 8 caracteres");
+        }
+           if (dni == -1 || dni == null) {
+            throw new Exception("El DNI no puede ser nulo o estar vacio");
         }
         if (correo.isEmpty() || correo == null) {
             throw new Exception("El correo no puede ser nulo o estar vacio");
