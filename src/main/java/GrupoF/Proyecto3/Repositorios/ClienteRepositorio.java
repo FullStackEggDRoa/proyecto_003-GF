4a8372c61b0f696321f255ccf28dcd78bf50f74b
Azul Castroviejo <azulcastroviejoh3@gmail.com>
2023-09-10 14:55:27 -0300

    Cambio

--- src/main/java/GrupoF/Proyecto3/Repositorios/ClienteRepositorio.java
+++ src/main/java/GrupoF/Proyecto3/Repositorios/ClienteRepositorio.java
@@ -1,4 +1,4 @@
-/*package GrupoF.Proyecto3.Repositorios;
+package GrupoF.Proyecto3.Repositorios;
 
 import GrupoF.Proyecto3.entidades.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClienteRepositorio extends JpaRepository<Cliente, String> {
     
-}*/
+}
