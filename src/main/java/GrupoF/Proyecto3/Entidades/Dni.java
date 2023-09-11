de198e509cf9da4aa128be84a04beccd73cd9cf6
ENRI644 <cre_comercial-094@hotmail.com>
2023-09-11 19:17:44 -0300

    demo semana 1

--- src/main/java/GrupoF/Proyecto3/Entidades/Dni.java
+++ src/main/java/GrupoF/Proyecto3/Entidades/Dni.java
@@ -1,14 +1,11 @@
 package GrupoF.Proyecto3.Entidades;
 
-<<<<<<< HEAD
-=======
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
->>>>>>> developer
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -22,16 +19,11 @@ import org.hibernate.annotations.GenericGenerator;
 @AllArgsConstructor
 @ToString
 
-<<<<<<< HEAD
-public class Dni{
-
-=======
 public class Dni implements Serializable {
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     @Column(name = "iddni",unique = true)
->>>>>>> developer
     private String id;
     @Column(name="serie")
     private char serie;
