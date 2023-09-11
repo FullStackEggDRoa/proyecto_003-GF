81d741e82d2de1870a4db073bc4125f1c5cacd05
ENRI644 <cre_comercial-094@hotmail.com>
2023-09-11 03:13:14 -0300

    demo 1

--- src/main/java/GrupoF/Proyecto3/Entidades/Dni.java
+++ src/main/java/GrupoF/Proyecto3/Entidades/Dni.java
@@ -1,14 +1,12 @@
 
 package GrupoF.Proyecto3.Entidades;
 
-import javax.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
