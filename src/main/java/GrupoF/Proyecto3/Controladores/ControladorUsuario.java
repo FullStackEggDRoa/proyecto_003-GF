01134e2168ba908f83c6aae78c09882910c07da6
ENRI644 <cre_comercial-094@hotmail.com>
2023-09-14 23:59:07 -0300

    primeros controladores

--- src/main/java/GrupoF/Proyecto3/Controladores/ControladorUsuario.java
+++ src/main/java/GrupoF/Proyecto3/Controladores/ControladorUsuario.java
@@ -3,6 +3,7 @@ package GrupoF.Proyecto3.Controladores;
 import GrupoF.Proyecto3.Servicios.ClienteServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,11 +23,13 @@ public class ControladorUsuario {
     
     @PostMapping("/registrar")
     public String registrar(@RequestParam String nombreApellido, @RequestParam String contraseña, @RequestParam String dni,
-            @RequestParam String correo, @RequestParam String telefono, @RequestParam String direccion){
+            @RequestParam String correo, @RequestParam String telefono, @RequestParam String direccion, ModelMap modelo){
         
         try {
             us.registrarCliente(nombreApellido, contraseña, dni, correo, telefono, direccion);
+            modelo.put("exito","El usuario se grabo correctamente");
         } catch (Exception e) {
+            modelo.put("error", e.getMessage());
             return "registro-usuario.html";
         }
         
