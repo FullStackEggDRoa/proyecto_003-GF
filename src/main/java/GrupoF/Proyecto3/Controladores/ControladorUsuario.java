d79af5c8e0aa3e16260932991c377ee7ddb5bbf5
ENRI644 <cre_comercial-094@hotmail.com>
2023-09-14 22:38:21 -0300

    primeros controladores

---
+++ src/main/java/GrupoF/Proyecto3/Controladores/ControladorUsuario.java
@@ -0,0 +1,37 @@
+package GrupoF.Proyecto3.Controladores;
+
+import GrupoF.Proyecto3.Servicios.ClienteServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/usuario")
+public class ControladorUsuario {
+
+    @Autowired
+    private ClienteServicio us;
+    
+    @GetMapping("/registro")
+    public String registro(){
+        return "registro-usuario.html";
+    }
+    
+    @PostMapping("/registrar")
+    public String registrar(@RequestParam String nombreApellido, @RequestParam String contraseña, @RequestParam String dni,
+            @RequestParam String correo, @RequestParam String telefono, @RequestParam String direccion){
+        
+        try {
+            us.registrarCliente(nombreApellido, contraseña, dni, correo, telefono, direccion);
+        } catch (Exception e) {
+            return "registro-usuario.html";
+        }
+        
+        
+        return ("index.html");
+    }
+    
+}
