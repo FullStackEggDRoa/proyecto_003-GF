ce086f132220c7eb3ec490edc0dfb15b6b0f71e5
Daniel Roa <119772142+dnroar@users.noreply.github.com>
2023-09-16 23:34:10 -0500

    Update ImagenServicio.java
    Reversión Manual de Cambios.

--- src/main/java/GrupoF/Proyecto3/Servicios/ImagenServicio.java
+++ src/main/java/GrupoF/Proyecto3/Servicios/ImagenServicio.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-/*
+
   
 @Service
 public class ImagenServicio {
@@ -72,6 +72,6 @@ public class ImagenServicio {
 		return imagenRepositorio.findAll();
 	}
   
-*/
+
 
 
