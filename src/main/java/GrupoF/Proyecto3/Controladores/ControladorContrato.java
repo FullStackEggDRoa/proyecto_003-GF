a41b4e52a0ef0fd6570d32a2a2a3eda7c12bb1a1
MoniFerVivas <118860376+MoniFerVivas@users.noreply.github.com>
2023-09-27 17:30:32 -0500

    Cambios Controlador Contrato con lo solicitado

--- src/main/java/GrupoF/Proyecto3/Controladores/ControladorContrato.java
+++ src/main/java/GrupoF/Proyecto3/Controladores/ControladorContrato.java
@@ -1,9 +1,9 @@
 package GrupoF.Proyecto3.Controladores;
 
-import GrupoF.Proyecto3.Servicios.ClienteServicio;
+import GrupoF.Proyecto3.Excepciones.MiExcepcion;
 import GrupoF.Proyecto3.Servicios.ContratoServicio;
-import GrupoF.Proyecto3.Servicios.ProveedorServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,19 +14,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/contratos")
 public class ControladorContrato {
     
-    /*@Autowired
-    private ProveedorServicio pS;
-    @Autowired
-    private ClienteServicio cS;*/
     @Autowired
     private ContratoServicio coS;
-        
+    
+@PreAuthorize("hasAnyRole('ROLE_USUARIO')")    
     @PostMapping("/contratar")
     public  String registrarContrato(@RequestParam String idCliente, @RequestParam String idProveedor, ModelMap modelo) {
         try {
             coS.registrarContrato(idCliente, idProveedor);
             modelo.put("exito","Contrato registrado exitosamente.");
-        } catch (Exception e) {
+        } catch (MiExcepcion e) {
             modelo.put("error", "Error al registrar el contrato.");
         }
         return ("sesion-cliente.html");
