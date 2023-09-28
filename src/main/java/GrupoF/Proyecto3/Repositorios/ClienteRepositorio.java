b97b3e4a60bdf6b09922de9b529c34d9cd51ab1e
Daniel Roa <119772142+dnroar@users.noreply.github.com>
2023-09-27 23:28:14 -0500

    Revert "Developer mv"

--- src/main/java/GrupoF/Proyecto3/Repositorios/ClienteRepositorio.java
+++ src/main/java/GrupoF/Proyecto3/Repositorios/ClienteRepositorio.java
@@ -1,7 +1,6 @@
 package GrupoF.Proyecto3.Repositorios;
 
 import GrupoF.Proyecto3.Entidades.Cliente;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +11,5 @@ public interface ClienteRepositorio extends JpaRepository<Cliente, String> {
     
     @Query("SELECT c FROM Cliente c WHERE c.correo = :correo")
     public Cliente buscarPorCorreo(@Param("correo") String correo);
-    
-    @Query("SELECT c FROM Cliente c ORDER BY c.nombreApellido")
-    public List<Cliente> listarClientesPorNombre();
 }
 
